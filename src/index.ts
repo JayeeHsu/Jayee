4be0eebf9644d6ce98763e59b3b987065643cd95
xujiayi03 <xujiayi03@kuaishou.com>
2023-02-08 15:11:14 +0800

    chore: change name

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { PluginOption } from "vite";
+
+const VitePluginVitePress = (): PluginOption => {
+  return {
+    name: "vite-plugin-vitepress",
+  };
+};
+
+export { VitePluginVitePress };
+
+export default VitePluginVitePress;
